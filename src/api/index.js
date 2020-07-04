2f9a7edc5cba5a00ffd331896a58ef9ec3d5cf46
kudzanayi <kdzvairo@gmail.com>
2020-07-04 17:11:15 -0400

    cards and charts

--- src/api/index.js
+++ src/api/index.js
@@ -1,7 +1,6 @@
 const url = 'https://covid19.mathdro.id/api'
 
 export const fetchData = async() => {
-
     try{
         const response = await fetch(url)
         const {confirmed, recovered, deaths, lastUpdate}   = await response.json()
@@ -11,4 +10,22 @@ export const fetchData = async() => {
 
     }
 
+}
+
+
+export const fetchDailyData = async () => {
+    try {
+        const response = await fetch(`${url}/daily`)
+        const data = await response.json()
+
+       const modifiedData = data.map((dailyData) => ({
+           confirmed: dailyData.confirmed.total,
+           deaths: dailyData.deaths.total,
+           date: dailyData.reportDate,
+       }))
+
+      return modifiedData
+    } catch (error){
+
+    }
 }
